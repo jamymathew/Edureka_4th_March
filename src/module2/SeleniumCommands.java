e452303e59dd64feafaf2afeaf7ebd674f417e94
tabhresh <tabhresh@gmail.com>
2019-03-08 16:50:15 +0530

    8th March 2019

--- src/module2/SeleniumCommands.java
+++ src/module2/SeleniumCommands.java
@@ -5,58 +5,61 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.safari.SafariDriver;
 
 public class SeleniumCommands {
 
 	public static void main(String[] args) {
+		// Single Line Comments
 		/*
-		 * Multi Line comments
-		 */		
-		//Single Line Comment
-		
-		//Basic Commands of Selenium Webdriver
-		
-		//Open Browser or Innitialize or Instantiate a browser
-		
+		 * Multiline comment
+		 */
+
+		// Instantiating a Browser
+
+		// Chrome
 		System.setProperty("webdriver.chrome.driver", "E:\\Abhresh\\Installation_stuff\\ExeFiles\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		
+
+		// Firefox
 		System.setProperty("webdriver.gecko.driver", "E:\\Abhresh\\Installation_stuff\\ExeFiles\\geckodriver.exe");
-		WebDriver driver1 = new FirefoxDriver();
-		
+		WebDriver FF = new FirefoxDriver();
+
+		// IE
 		System.setProperty("webdriver.ie.driver", "E:\\Abhresh\\Installation_stuff\\ExeFiles\\IEDriverServer.exe");
-		WebDriver driver2 = new InternetExplorerDriver();
+		WebDriver IE = new InternetExplorerDriver();
 
-		//Close and Quit Commands
-		driver.close();// it will close the current active window in the current session
-		driver.quit();// it will close all the windows in the current session
-		
-		//Get Commands
-		driver.get("URL");// this is used to open the URL of the AUT
-		driver.getCurrentUrl();// this will capture the current URL of that page on which you are at present
-		driver.getTitle();//this will capture the current page title.
-		driver.getWindowHandle();//this will get me the names of the windows
+		// Safari
+		WebDriver safari = new SafariDriver();
 
-		//Navigation Commands
-		driver.navigate().to("String/URL");// this will be used to move toward either an object or towards a URL
-		driver.navigate().back();//to move to a previous page
-		driver.navigate().forward();//to move to the next page (already excuted)
-		driver.navigate().refresh();//to refresh the page
-		
-		//Element handling commands
-		driver.findElement(By.id("")).clear();// to clear the field
-		driver.findElement(By.id("")).click();// to click on an element
-		driver.findElement(By.id("")).sendKeys("Abhresh");// to type on and text box
-		
+		// Close and Quit Browser
+		driver.close();// this will close the current active window of the current instance
+		driver.quit();// this will close all the windows in the current instance
+
+		// Get Commands
+		driver.get("URL");// Open URL of an AUT
+		driver.getTitle(); // This is will help you to fetch the title of the current page
+		driver.getCurrentUrl();// This will help you to fetch the URL of the current Page
+		driver.getWindowHandle(); // This will help you to handle the multiple windows which is going to be
+									// discussed in the 5th module
+		driver.getPageSource(); // This is used to get the page source of the current URL and it is displayed in
+								// the console panel
+		driver.getClass(); // this is used to get the class name of the current Element
+
+		// Navigation Commands
+		driver.navigate().to("URL");// Navigating to a perticular URL and it can be same as driver.get commands
+		driver.navigate().back();// to go to previous page
+		driver.navigate().forward();// to move to the next page
+		driver.navigate().refresh();// to refresh the page
+
+		// Element Handling Commands
+		// Click
+		driver.findElement(By.id("")).click();
+		// Type
+		driver.findElement(By.id("")).sendKeys("Abhresh");
+		// Clear
+		driver.findElement(By.id("")).clear();
+
 	}
 
 }
-
-
-
-
-
-
-
-
-
